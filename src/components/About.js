61a77e4c015e2f3249e09350b6bcc185930b14d8
pawan5557 <keswanipawan68@gmail.com>
2025-12-28 20:17:58 +0530

    Initial commit

---
+++ src/components/About.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div class="lead mx-2">
+      <p>
+      Analyze Your text with Textutils!</p>
+<p>Textutils gives you a way to analyze your text quickly and efficiently. Be it word count, character count or
+Free to use</p>
+<p>Textutils is a free character counter tool that provides instant character count & word count statistics for a given
+text. TextUtils reports the number of words and characters. Thus it is suitable for writing text with word/ character
+limit</p>
+<p>Browser Compatible
+This word counter software works in any web browsers such as Chrome, Firefox, Internet Explorer, Safari, Opera. It
+suits to count characters in facebook, blog, books, excel document, pdf document, essays, etc.
+</p>
+    </div>
+  )
+}
